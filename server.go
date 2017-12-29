14ff0ed9654796443aad270bd42732a6fa50ee4e
Brian Trammell <brian@trammell.ch>
2017-12-29 21:43:39 +0100

    remove xss hack

--- server.go
+++ server.go
@@ -155,7 +155,6 @@ func (pa *PingAPI) HandlePingRetrieve(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.WriteHeader(http.StatusOK)
 	streamCopy(infile, w)
 }
